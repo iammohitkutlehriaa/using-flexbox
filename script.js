4f24076f46ea150f6a2aa054cd42f576bcb1da07
Mohit <167785696+iammohitkutlehriaa@users.noreply.github.com>
2024-12-18 23:57:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let items = {
+    name: "mohit_kutlehria",
+    class: "2nd_bca_sem",
+    age: "21",
+}
+items.number= "9015378389";
+items.is_Follow = "true";
+items.age = "22";
+console.log(items)
